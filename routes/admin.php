f73e4cb889d46416fe0ca19b431903923a6847b3
abbascamp <abbas.camp@payl8r.com>
2022-06-19 15:59:42 +0100

    - Removed admin route prefix

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function() {
-    Route::resource('categories', CategoryController::class);
-});
+Route::resource('categories', CategoryController::class);
